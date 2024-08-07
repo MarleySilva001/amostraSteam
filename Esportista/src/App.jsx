241f2cf3ec3a3ea4879ffb4f994fc37784ecb3c5
MARLEY CLYFF BONIFACIO DA SILVA <marley_c_silva@estudante.sesisenai.org.br>
2024-08-07 12:31:49 -0300

    estruturando o projeto

---
+++ Esportista/src/App.jsx
@@ -0,0 +1,18 @@
+import { Router, Routes, Route } from "react-router-dom";
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={} />
+      </Routes>
+    </Router>
+    </>
+  )
+}
+
+export default App
