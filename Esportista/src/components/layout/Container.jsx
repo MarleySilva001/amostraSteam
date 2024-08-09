84b0842b5f815a4fc2044fb795635d321398025d
MarleySilva001 <129191771+MarleySilva001@users.noreply.github.com>
2024-08-09 02:29:05 +0000

    estrutura da SplashScreen

---
+++ Esportista/src/components/layout/Container.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './Container.module.css';
+
+const Container = (props) => {
+    return (
+        <div className={style.center}>{props.children}</div>
+    );
+}
+
+export default Container;
--- Esportista/src/pages/SplashScreen.jsx
+++ Esportista/src/pages/SplashScreen.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { motion } from "framer-motion";
+import Container from '../components/layout/Container';
 
 const SplashScreen = () => {
+
+    let Navigate = useNavigate()
+    const [Carregando, setCarregando] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setCarregando(false)
+        }, 6000)
+        return () => {
+            clearTimeout(timer)
+        };
+    }, []);
+
     return (
-        <div>
-            SplashScreen
-        </div>
+        <>
+            <Container>
+                {Carregando ? (
+                    <motion.div
+                        initial={{ opacity: 0, scale: 0.1 }}
+                        animate={{ opacity: 1, scale: 1  }}
+                        transition={{ duration: 3 }}
+                    >
+                        SplashScreen
+                    </motion.div>
+                ) : (
+                    Navigate('/home')
+                )}
+
+            </Container>
+        </>
     );
 }
 
